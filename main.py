0e01c3990745805cb6f38b015a3619ecf26d617d
ddilshod2197 <ddilshod2197@gmail.com>
2026-01-05 12:37:34 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import harorat
+
+c = 25
+f = 77
+
+res1 = harorat.celsius_fahrenheit(c)
+print(res1)
+
+res2 = harorat.fahrenheit_celsius(f)
+print(res2)
+
+res3 = harorat.celsius_kelvin(c)
+print(res3)
